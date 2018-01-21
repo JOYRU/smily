8bff489bbc90e59b125a324e869515f6a7319326
JOY_RU <joycseru@gmail.com>
2018-01-22 00:29:01 +0600

    implement method initialPlayerCards

--- src/joy/com/ru/Game.java
+++ src/joy/com/ru/Game.java
@@ -1,4 +1,6 @@
 package joy.com.ru;
+import java.io.*;
+import java.util.Scanner;
 
 public class Game {
 	
@@ -11,5 +13,18 @@ private static int playercard1, playercard2, dealercard1, dealercard2, playercar
 		playercardtotal +=playercard;
 		System.out.println("Your score is " + playercardtotal);
 	}
+	
+	
+	public static void initialPlayerCards()
+	{
+		System.out.println("Welcome To Simple  Blackjack Game!");
+		playercard1 = (int) (Math.random() * 9 + 2);
+		playercard2 = (int) (Math.random() * 9 + 2);
+		System.out.println("You drew a " + playercard1 + " and a " + playercard2 + ".");
+		playercardtotal = playercard1 + playercard2;
+		System.out.println("Your total is " + playercardtotal + ".");
+		System.out.println();
+	}
+	
 
 }
