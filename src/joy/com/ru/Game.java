d713b7065f274fdb4f269e44de5bf46951e8fc56
JOY_RU <joycseru@gmail.com>
2018-01-22 00:26:09 +0600

    first implement playerhit method

--- src/joy/com/ru/Game.java
+++ src/joy/com/ru/Game.java
@@ -1,5 +1,15 @@
 package joy.com.ru;
 
 public class Game {
+	
+private static int playercard1, playercard2, dealercard1, dealercard2, playercardtotal, dealercardtotal;
+	
+	public static void playerhit()
+	{
+		int playercard = (int) (Math.random() * 9 + 2);
+		System.out.println("You drew a " + playercard + ".");
+		playercardtotal +=playercard;
+		System.out.println("Your score is " + playercardtotal);
+	}
 
 }
